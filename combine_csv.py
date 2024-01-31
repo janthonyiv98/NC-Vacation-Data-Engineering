a38e2b5cb4dec255d288caa631b914cea7fb6894
rhett-gough <rhett.gough@gmail.com>
2024-01-31 15:34:01 -0500

    moved id columns to the front

--- combine_csv.py
+++ combine_csv.py
@@ -19,24 +19,6 @@ for csv in csv_files:
         # Try reading the file using default UTF-8 encoding
         df = pd.read_csv(file_path)
         df_list.append(df)
-
-
-        # add identifier numbers
-        df.loc[df['name'].str.contains('Asheville', case=False, na=False), 'NC_ID'] = 'nc01'
-        df.loc[df['name'].str.contains('Banner Elk', case=False, na=False), 'NC_ID'] = 'nc02'
-        df.loc[df['name'].str.contains('Boone', case=False, na=False), 'NC_ID'] = 'nc03'
-        df.loc[df['name'].str.contains('Charlotte', case=False, na=False), 'NC_ID'] = 'nc04'
-        df.loc[df['name'].str.contains('Cherokee', case=False, na=False), 'NC_ID'] = 'nc05'
-        df.loc[df['name'].str.contains('Emerald Isle', case=False, na=False), 'NC_ID'] = 'nc06'
-        df.loc[df['name'].str.contains('High Point', case=False, na=False), 'NC_ID'] = 'nc07'
-        df.loc[df['name'].str.contains('Nags Head', case=False, na=False), 'NC_ID'] = 'nc08'
-        df.loc[df['name'].str.contains('Ocracoke', case=False, na=False), 'NC_ID'] = 'nc09'
-        df.loc[df['name'].str.contains('raleigh', case=False, na=False), 'NC_ID'] = 'nc10'
-        df.loc[df['name'].str.contains('Wilmington', case=False, na=False), 'NC_ID'] = 'nc11'
-        df.loc[df['name'].str.contains('Winston-Salem', case=False, na=False), 'NC_ID'] = 'nc12'
-
-        df_list.append(df)
-           
            
     except UnicodeDecodeError:
         try:
@@ -54,5 +36,26 @@ combined_df = pd.concat(df_list, ignore_index=True)
 # create a new index
 combined_df["id"] = combined_df.index + 1
 
+ # add identifier numbers
+combined_df.loc[combined_df['name'].str.contains('Asheville', case=False, na=False), 'NC_ID'] = 'nc01'
+combined_df.loc[combined_df['name'].str.contains('Banner Elk', case=False, na=False), 'NC_ID'] = 'nc02'
+combined_df.loc[combined_df['name'].str.contains('Boone', case=False, na=False), 'NC_ID'] = 'nc03'
+combined_df.loc[combined_df['name'].str.contains('Charlotte', case=False, na=False), 'NC_ID'] = 'nc04'
+combined_df.loc[combined_df['name'].str.contains('Cherokee', case=False, na=False), 'NC_ID'] = 'nc05'
+combined_df.loc[combined_df['name'].str.contains('Emerald Isle', case=False, na=False), 'NC_ID'] = 'nc06'
+combined_df.loc[combined_df['name'].str.contains('High Point', case=False, na=False), 'NC_ID'] = 'nc07'
+combined_df.loc[combined_df['name'].str.contains('Nags Head', case=False, na=False), 'NC_ID'] = 'nc08'
+combined_df.loc[combined_df['name'].str.contains('Ocracoke', case=False, na=False), 'NC_ID'] = 'nc09'
+combined_df.loc[combined_df['name'].str.contains('raleigh', case=False, na=False), 'NC_ID'] = 'nc10'
+combined_df.loc[combined_df['name'].str.contains('Wilmington', case=False, na=False), 'NC_ID'] = 'nc11'
+combined_df.loc[combined_df['name'].str.contains('Winston-Salem', case=False, na=False), 'NC_ID'] = 'nc12'
+
+# Move columns to front
+first_column = combined_df.pop('id')
+second_column = combined_df.pop('NC_ID')
+
+combined_df.insert(0, 'id', first_column)
+combined_df.insert(1, 'NC_ID', second_column)
+
 # Save the final result to a new CSV file
-combined_df.to_csv(os.path.join(folder_path, 'combined_file.csv'), index=False)
+combined_df.to_csv('combined_data.csv',encoding='utf-8', index=False)
